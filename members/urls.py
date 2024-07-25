5a7c7ef3bd639fe3af82ed58d116dc104bab9065
Djole33 <pandajedetrsku@gmail.com>
2024-07-25 17:44:19 +0200

    added edit and show profile functions

--- members/urls.py
+++ members/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit-profile/', UserUpdateView.as_view(), name='edit-profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
-    path('password-success/', password_success, name="password-success"),
-    
+    path('password-success', password_success, name="password-success"),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="show-profile-page"),
+    path('<int:pk>/edit-profile-page/', EditProfilePageView.as_view(), name="edit-profile-page"),
 ]
--- members/views.py
+++ members/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from theblog.models import Profile
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pinterest_url']
+    success_url = reverse_lazy('home')
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+    def get_context_data(self, *args, **kwargs):
+            context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+            page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+            context["page_user"] = page_user
+            return context
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
